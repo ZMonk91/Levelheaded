3a68c80376f1fb2a8967bf634cf412cd95d3e23c
Zachary Monk <ZMonk91@Gmail.com>
2023-10-30 18:31:23 -0700

    Initial Commit

---
+++ Level-Headed/Level_HeadedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Level_HeadedApp.swift
+//  Level-Headed
+//
+//  Created by Zachary Monk on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Level_HeadedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
